6c172682baf95e2adb85424221895381c3990a30
Gil Mor <37264700+Gil-Mor97@users.noreply.github.com>
2019-07-23 12:38:30 +0300

    xml

--- task2_GilMor_AnnaStrijko/task2_GilMor_AnnaStrijko/Default.aspx.cs
+++ task2_GilMor_AnnaStrijko/task2_GilMor_AnnaStrijko/Default.aspx.cs
@@ -16,7 +16,7 @@ public partial class _Default : System.Web.UI.Page
 
     protected void Button1_Click(object sender, EventArgs e)
     {
-        XmlTextWriter a = new XmlTextWriter(Server.MapPath("XMLFile.xml"), System.Text.Encoding.UTF8);
+        XmlTextWriter a = new XmlTextWriter(Server.MapPath("MyXMLFile.xml"), System.Text.Encoding.UTF8);
         a.WriteStartDocument();
 
         int gameID = 1;
@@ -27,14 +27,16 @@ public partial class _Default : System.Web.UI.Page
 
         a.WriteStartElement("game");
         a.WriteAttributeString("id", gameID.ToString());
+        a.WriteAttributeString("gamecode", codeTB.Text);
+        a.WriteAttributeString("category", categoryTB.Text);
 
-        a.WriteElementString("question", "בחרו אך ורק בבעלי חיים ממשפחת היונקים");
-        a.WriteAttributeString("id", (gameID * 100).ToString());
+        a.WriteElementString("question", qtnTB.Text);
+        a.WriteAttributeString("id", (gameID * 100) + "");
 
-        a.WriteStartElement("publishDate");
-        a.WriteAttributeString("pInHebrew", "false");
-        a.WriteString("21/7/19");
-        a.WriteEndElement();
+        a.WriteElementString("answer", contentTB.Text);
+        a.WriteAttributeString("id", (gameID + 100).ToString());
+        a.WriteAttributeString("qType", typeRBL.SelectedValue);
+        a.WriteAttributeString("isCorrect", correctRBL.SelectedValue);
 
         a.WriteEndElement();
         a.WriteEndElement();
